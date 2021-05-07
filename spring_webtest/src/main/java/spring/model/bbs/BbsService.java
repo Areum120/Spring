7fe20c63dcf90ee69092367c7321a8caa0bd1d49
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-05-07 22:53:54 +0900

    Bbs DAO, DTO, Mapper update

---
+++ spring_webtest/src/main/java/spring/model/bbs/BbsService.java
@@ -0,0 +1,29 @@
+package spring.model.bbs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.model.reply.ReplyMapper;
+
+@Service
+public class BbsService {
+	
+	@Autowired
+	private BbsMapper mapper;
+	@Autowired
+	private ReplyMapper rmapper;
+	@Autowired
+	private BbsDAOJPA jpa;
+	
+	public void insert(BbsVO vo) {
+		jpa.insertBbs(vo);
+	}
+	
+	public void delete(int bbsno) throws Exception {	
+		//댓글들을 지운다.
+		rmapper.bdelete(bbsno);		
+		//부모글 을 지운다.
+		mapper.delete(bbsno);
+	}
+
+}
